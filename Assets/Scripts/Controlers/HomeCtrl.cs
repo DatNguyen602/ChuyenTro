e4d1fa1ded848d7090895e15cdf31012bc7ca0b2
DatNguyen602 <109007200+DatNguyen602@users.noreply.github.com>
2025-07-23 10:46:21 +0700

    t01

--- Assets/Scripts/Controlers/HomeCtrl.cs
+++ Assets/Scripts/Controlers/HomeCtrl.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class HomeCtrl : MonoBehaviour
 {
     [SerializeField] private Transform boardTransform, btnCtrls;
     [SerializeField] private Color selectedColor = Color.green;
+    [SerializeField] private GameObject LoadingUI;
 
     public void CloseAllBoard()
     {
@@ -35,6 +37,31 @@ public class HomeCtrl : MonoBehaviour
         if(img)
         {
             img.color = selectedColor;
+        }
+    }
+
+    private float timer = 0.0f;
+    public void LoadingScene(string sceneName)
+    {
+        timer = 5.0f;
+        AsyncOperation scene = SceneManager.LoadSceneAsync(sceneName);
+        if (LoadingUI) LoadingUI.SetActive(true);
+        if (scene != null)
+        {
+            scene.allowSceneActivation = false;
+            while (!scene.isDone)
+            {
+                if (scene.progress >= 0.9f && timer <= 0)
+                {
+                    scene.allowSceneActivation = true;
+                    //if (LoadingUI) LoadingUI.SetActive(false);
+                    return;
+                }
+                else
+                {
+                    timer -= Time.deltaTime;
+                }
+            }
         }
     }
 
