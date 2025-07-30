e6cdd56a8a0ad1f7c02973bc36c9962c52049c89
Lê Đức Hùng <leduchung27092004@gmail.com>
2025-07-30 11:40:29 +0700

    SelectUniversity
    SelectUniversity

---
+++ Assets/Scripts/UniversityDatas/UniversitySelector.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UniversitySelector : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI universityNameTMP;
+    [SerializeField] private SpriteRenderer logoSpriteRenderer;
+
+    [SerializeField] private Button nextButton;
+    [SerializeField] private Button previousButton;
+
+    private UniversityData[] universityDatas;
+    private int currentIndex = 0;
+
+    void Start()
+    {
+        universityDatas = Resources.LoadAll<UniversityData>("UniversityDatas");
+
+        if (universityDatas.Length > 0)
+        {
+            ShowUniversity(currentIndex);
+        }
+
+        nextButton.onClick.AddListener(ShowNextUniversity);
+        previousButton.onClick.AddListener(ShowPreviousUniversity);
+
+        ShowUniversity(currentIndex);
+    }
+
+    public void ShowNextUniversity()
+    {
+        if (universityDatas.Length == 0) return;
+        currentIndex = (currentIndex + 1) % universityDatas.Length;
+        ShowUniversity(currentIndex);
+    }
+
+    public void ShowPreviousUniversity()
+    {
+        if (universityDatas.Length == 0) return;
+        currentIndex = (currentIndex - 1 + universityDatas.Length) % universityDatas.Length;
+        ShowUniversity(currentIndex);
+    }
+
+    private void ShowUniversity(int index)
+    {
+        UniversityData data = universityDatas[index];
+        universityNameTMP.text = data.universityName;
+        logoSpriteRenderer.sprite = data.logo;
+    }
+}
